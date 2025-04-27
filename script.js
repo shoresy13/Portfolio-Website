b88af901473930f0a9b72e521e4a9bf30e408a37
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2025-04-27 15:33:24 +0100

    Change scroll behaviour

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const navLinks = document.querySelectorAll(".navbar-links a");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const aboutLink = document.querySelector('a[href="#about"]');
+  aboutLink.classList.add("active");
+
+  const targetSection = document.querySelector("#about");
+  targetSection.scrollIntoView({ behavior: "smooth" });
+
+  history.replaceState(null, "", "#about");
+});
+
 navLinks.forEach((link) => {
-  link.addEventListener("click", function () {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
     navLinks.forEach((link) => link.classList.remove("active"));
     this.classList.add("active");
+
+    const targetSection = document.querySelector(this.getAttribute("href"));
+    targetSection.scrollIntoView({ behavior: "smooth" });
+
+    history.pushState(null, "", this.getAttribute("href"));
   });
 });
