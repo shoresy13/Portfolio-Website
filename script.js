e70dc9dcc0d37e57e2f6e63388c8c75cd1933e1e
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2025-04-27 14:39:14 +0100

    Add navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll(".navbar-links a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
